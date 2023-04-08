0250d11ea689838497de2692dc1af44555d1158a
tyeb11 <tayyeb.merchant@gmail.com>
2023-04-08 13:40:21 +0530

    controller(add): label no more multiple reply

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,10 @@ import chalk from "chalk";
 import { google } from "googleapis";
 import nodemailer from "nodemailer";
 import { __dirname } from "../app.js";
+import axios from "axios";
+
+const repliedEmails = [];
+
 const oAuth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
@@ -22,8 +26,9 @@ export async function getReplyToCurrentEmails(req, res) {
     const { data } = await gmail.users.messages.list({
       userId: req.user.id,
       q: "after:2023/04/04 before:2023/05/04 is:inbox label:unread category:primary -in:chats -from:me -subject:Re: -subject:Fwd:",
-      maxResults: 10,
+      maxResults: 5,
     });
+
     const emails = data.messages;
     if (emails.length == 0) {
       res.send("No Email found");
@@ -35,9 +40,10 @@ export async function getReplyToCurrentEmails(req, res) {
         id: email.id,
       });
       const headers = message.data.payload.headers;
-      if (
-        headers.filter((header) => header.name === "In-Reply-To").length === 0
-      ) {
+      // console.log(chalk.gray(JSON.stringify(message.data)));
+      console.log();
+
+      if (!repliedEmails.includes(email.threadId)) {
         const to = headers.find((header) => header.name === "To").value;
         const from = headers.find((header) => header.name === "From").value;
         const subject = headers.find(
@@ -66,8 +72,24 @@ export async function getReplyToCurrentEmails(req, res) {
           },
         });
         await transport.sendMail(mailOptions);
+        repliedEmails.push(email.threadId);
         console.log(chalk.yellow(`mail delivered to : ${to}`));
       }
+      const { data } = await gmail.users.messages.list({
+        userId: req.user.id,
+        q: "label:sent",
+        maxResults: 5,
+      });
+      data.messages.forEach(async (mail) => {
+        await gmail.users.messages.modify({
+          auth: oAuth2Client,
+          userId: req.user.id,
+          id: mail.id,
+          resource: {
+            addLabelIds: ["Label_1"],
+          },
+        });
+      });
     });
     res.sendFile(__dirname + "/index.html");
   } catch (e) {
